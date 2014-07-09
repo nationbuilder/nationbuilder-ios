85cf9d2d200ee3c4947429878a94f1c3f9f4479a
Peng Wang <peng@nationbuilder.com>
2014-07-08 18:55:26 -0700

    Add empty testing application

---
+++ NBClientCatalogue/NBClientCatalogue/NBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NBAppDelegate.h
+//  NBClientCatalogue
+//
+//  Created by Peng Wang on 7/8/14.
+//  Copyright (c) 2014 NationBuilder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
