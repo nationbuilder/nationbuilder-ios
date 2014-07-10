3594b62d28c3bb467cd8d8f0b16e14138459de99
Peng Wang <peng@nationbuilder.com>
2014-07-10 12:32:40 -0700

    Add main header to client project

---
+++ NBClient/NBClient/Main.h
@@ -0,0 +1,14 @@
+//
+//  Main.h
+//  NBClient
+//
+//  Created by Peng Wang on 7/10/14.
+//  Copyright (c) 2014 NationBuilder. All rights reserved.
+//
+
+#ifndef _NBCLIENT_
+    #define _NBCLIENT_
+
+    #import "NBClient.h"
+
+#endif /* _NBCLIENT_ */
