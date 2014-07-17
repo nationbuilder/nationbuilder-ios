48df4d8204e0ce7ff8b2e57faa9c6a5c7d7e87bd
Peng Wang <peng@nationbuilder.com>
2014-07-16 19:30:06 -0700

    Add client fetch people methods tests

---
+++ NBClient/NBClientTests/NBClientPeopleTests.m
@@ -0,0 +1,143 @@
+//
+//  NBClientPeopleTests.m
+//  NBClient
+//
+//  Created by Peng Wang on 7/16/14.
+//  Copyright (c) 2014 NationBuilder. All rights reserved.
+//
+
+#import "NBTestCase.h"
+
+#import "Main.h"
+
+@interface NBClientPeopleTests : NBTestCase
+
+@property (strong, nonatomic) NBClient *client;
+
+- (void)assertPeopleArray:(NSArray *)array;
+- (void)assertPersonDictionary:(NSDictionary *)dictionary;
+- (void)assertServiceError:(NSError *)error;
+- (void)assertSessionDataTask:(NSURLSessionDataTask *)task;
+
+@end
+
+@implementation NBClientPeopleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // We need to use the shared session because we need to be in an application
+    // for an app-specific cache.
+    self.client = [[NBClient alloc] initWithNationName:self.nationName
+                                                apiKey:self.apiKey
+                                      customURLSession:[NSURLSession sharedSession]
+                         customURLSessionConfiguration:nil];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+#pragma mark - Assertion Macros
+
+- (void)assertPeopleArray:(NSArray *)array
+{
+    XCTAssertNotNil(array,
+                    @"Client should have fetched list of people.");
+    for (NSDictionary *dictionary in array) {
+        [self assertPersonDictionary:dictionary];
+    }
+}
+
+- (void)assertPersonDictionary:(NSDictionary *)dictionary
+{
+    static NSArray *keys;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        keys = @[ @"email", @"id", @"first_name", @"last_name", @"support_level" ];
+    });
+    for (NSString *key in keys) {
+        XCTAssertNotNil(dictionary[key],
+                        @"Person dictionary should have value for %@", key);
+    }
+}
+
+- (void)assertServiceError:(NSError *)error
+{
+    if (!error || error.domain != NBErrorDomain) {
+        return;
+    }
+    if (error.code == NBClientErrorCodeService) {
+        XCTFail(@"People service returned error %@", error);
+    }
+}
+
+- (void)assertSessionDataTask:(NSURLSessionDataTask *)task
+{
+    XCTAssertTrue(task && task.state == NSURLSessionTaskStateRunning,
+                  @"Client should have created and ran task.");
+    NSLog(@"REQUEST: %@", task.currentRequest.nb_debugDescription);
+}
+
+#pragma mark - Tests
+
+- (void)testFetchPeople
+{
+    [self setUpAsync];
+    NSURLSessionDataTask *task =
+    [self.client fetchPeopleWithCompletionHandler:^(NSArray *items, NSError *error) {
+        [self assertServiceError:error];
+        [self assertPeopleArray:items];
+        [self completeAsync];
+    }];
+    [self assertSessionDataTask:task];
+    [self tearDownAsync];
+}
+
+- (void)testFetchPeopleByParameters
+{
+    [self setUpAsync];
+    NSURLSessionDataTask *task =
+    [self.client
+     fetchPeopleByParameters:@{ @"city": @"Los Angeles" }
+     withCompletionHandler:^(NSArray *items, NSError *error) {
+         [self assertServiceError:error];
+         [self assertPeopleArray:items];
+         [self completeAsync];
+     }];
+    [self assertSessionDataTask:task];
+    [self tearDownAsync];
+}
+
+- (void)testFetchPersonByIdentifier
+{
+    [self setUpAsync];
+    NSURLSessionDataTask *task =
+    [self.client
+     fetchPersonByIdentifier:self.userIdentifier
+     withCompletionHandler:^(NSDictionary *item, NSError *error) {
+        [self assertServiceError:error];
+        [self assertPersonDictionary:item];
+        [self completeAsync];
+    }];
+    [self assertSessionDataTask:task];
+    [self tearDownAsync];
+}
+
+- (void)testFetchPersonByParameters
+{
+    [self setUpAsync];
+    NSURLSessionDataTask *task =
+    [self.client
+     fetchPersonByParameters:@{ @"email" : self.userEmailAddress }
+     withCompletionHandler:^(NSDictionary *item, NSError *error) {
+         [self assertServiceError:error];
+         [self assertPersonDictionary:item];
+         [self completeAsync];
+     }];
+    [self assertSessionDataTask:task];
+    [self tearDownAsync];
+}
+
+@end
