9b5827806910462d78cecca8b5971f7de1e579e2
Peng Wang <peng@nationbuilder.com>
2014-07-18 16:37:00 -0700

    Document client people methods

--- NBClient/NBClient/NBClient.h
+++ NBClient/NBClient/NBClient.h
@@ -35,26 +35,32 @@ extern NSUInteger const NBClientErrorCodeService;
                   customURLSession:(NSURLSession *)urlSession
      customURLSessionConfiguration:(NSURLSessionConfiguration *)sessionConfiguration;
 
-// People
+/**
+ People
+ 
+ The endpoint annotations are for reference only to link to the API docs and may
+ change independently of the method selectors below.
+ */
 
+// GET /people
 - (NSURLSessionDataTask *)fetchPeopleWithCompletionHandler:(NBClientResourceListCompletionHandler)completionHandler;
-
+// GET /people/search
 - (NSURLSessionDataTask *)fetchPeopleByParameters:(NSDictionary *)parameters
                             withCompletionHandler:(NBClientResourceListCompletionHandler)completionHandler;
-
+// GET /people/:id
 - (NSURLSessionDataTask *)fetchPersonByIdentifier:(NSUInteger)identifier
                             withCompletionHandler:(NBClientResourceItemCompletionHandler)completionHandler;
-
+// GET /people/match
 - (NSURLSessionDataTask *)fetchPersonByParameters:(NSDictionary *)parameters
                             withCompletionHandler:(NBClientResourceItemCompletionHandler)completionHandler;
-
+// POST /people
 - (NSURLSessionDataTask *)createPersonWithParameters:(NSDictionary *)parameters
                                    completionHandler:(NBClientResourceItemCompletionHandler)completionHandler;
-
+// PUT /people/:id
 - (NSURLSessionDataTask *)savePersonByIdentifier:(NSUInteger)identifier
                                   withParameters:(NSDictionary *)parameters
                                completionHandler:(NBClientResourceItemCompletionHandler)completionHandler;
-
+// DELETE /people/:id
 - (NSURLSessionDataTask *)deletePersonByIdentifier:(NSUInteger)identifier
                              withCompletionHandler:(NBClientResourceItemCompletionHandler)completionHandler;
 
