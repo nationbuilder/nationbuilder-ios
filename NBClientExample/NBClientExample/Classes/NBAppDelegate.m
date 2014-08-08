230a49c8ca93e85f4f7cc37311d2771b8aa4595d
Peng Wang <peng@nationbuilder.com>
2014-08-07 18:31:40 -0700

    Rewrite how client gets configured
    Environment variables don't get sent when the app
    is run without the debugger. Rather than
    continuing down this route, find something less
    complex and hidden (launch arguments made sense
    for tests--supposed cli integration, whereas this
    is different) ...
    
    Use an Info.plist instead, with constant keys for
    safety. They are the conventional form of
    static configuration storage for apps.

--- NBClientExample/NBClientExample/Classes/NBAppDelegate.m
+++ NBClientExample/NBClientExample/Classes/NBAppDelegate.m
@@ -43,17 +43,24 @@
     if (_client) {
         return _client;
     }
-    NSDictionary *environment = [NSProcessInfo processInfo].environment;
-    if (environment[@"NBNationName"]) {
-        NSString *baseURLString = [NSString stringWithFormat:environment[@"NBBaseURLFormat"], environment[@"NBNationName"]];
-        NSURL *url = [NSURL URLWithString:baseURLString];
-        self.client = [[NBClient alloc] initWithNationName:environment[@"NBNationName"]
-                                                    apiKey:environment[@"NBClientAPIKey"]
-                                             customBaseURL:url
-                                          customURLSession:nil customURLSessionConfiguration:nil];
-    } else {
-        [[[UIAlertView alloc] initWithTitle:@"FAIL" message:@"FAIL" delegate:nil cancelButtonTitle:nil otherButtonTitles:nil] show];
+    NSString *path = [[NSBundle mainBundle] pathForResource:NBInfoFileName ofType:@"plist"];
+    NSAssert(path, @"%@.plist could not be found. Please add it and include the proper entries.", NBInfoFileName);
+    NSDictionary *infos = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSDictionary *info;
+#if defined(DEBUG) && TARGET_IPHONE_SIMULATOR
+    // NOTE: This configuration group is meant for internal use only, unless
+    // you have a development-specific set of NationBuilder configuration.
+    info = infos[NBInfoDevelopmentKey];
+#endif
+    if (!info) {
+        info = infos[NBInfoProductionKey];
     }
+    NSString *baseURLString = [NSString stringWithFormat:info[NBInfoBaseURLFormatKey], info[NBInfoNationNameKey]];
+    NSURL *url = [NSURL URLWithString:baseURLString];
+    self.client = [[NBClient alloc] initWithNationName:info[NBInfoNationNameKey]
+                                                apiKey:info[NBInfoTestTokenKey]
+                                         customBaseURL:url
+                                      customURLSession:nil customURLSessionConfiguration:nil];
     return _client;
 }
 
